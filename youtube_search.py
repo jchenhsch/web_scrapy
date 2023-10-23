bba499fd79de2f88a26bb75c62167314b773205c
Jiaxuan_Chen <714008556cjx@gmail.com>
2023-10-23 13:00:48 -0500

    experiment with pyppeteer to grab youtube comment and search it is more efficient in regards to content loading compared to selenium

---
+++ youtube_search.py
@@ -0,0 +1,44 @@
+import asyncio
+from pyppeteer import launch
+
+async def get_youtube_search_links(keyword):
+    browser = await launch(headless=True)
+    page = await browser.newPage()
+
+    # Navigate to the YouTube search results page with the specific keyword
+    search_url = f'https://www.youtube.com/results?search_query={"china"}'
+    await page.goto(search_url)
+
+    # Wait for the search results to load
+    await page.waitForSelector('#contents')
+
+    # Infinite scroll until all search results are loaded (adjust the number of scrolls as needed)
+    for _ in range(10):
+        await page.evaluate('window.scrollTo(0, document.documentElement.scrollHeight);')
+        await asyncio.sleep(2)  # Wait for 2 seconds between scrolls
+
+    # Extract video links from the search results
+    video_links = await page.evaluate('''() => {
+        const links = [];
+        const videoElements = document.querySelectorAll('#contents ytd-video-renderer');
+        videoElements.forEach(videoElement => {
+            const linkElement = videoElement.querySelector('a#thumbnail');
+            if (linkElement) {
+                const link = linkElement.getAttribute('href');
+                const videoIdMatch = link.match(/(\/watch\?v=)([a-zA-Z0-9_-]{11})/);
+                if (videoIdMatch && videoIdMatch[2]) {
+                    links.push(`https://www.youtube.com/watch?v=${videoIdMatch[2]}`);
+                }
+            }
+        });
+        return links;
+    }''')
+
+
+    # Close the browser
+    await browser.close()
+    print (video_links)
+    return video_links
+
+# Run the function and print the list of video links
+asyncio.get_event_loop().run_until_complete(get_youtube_search_links('china'))
